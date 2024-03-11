e35b907ce3216ff00c6bbab78f553871b1147d01
Tim Mitra <tim@it-guy.com>
2024-03-11 18:54:18 -0400

    Initial Commit

---
+++ Motion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Motion
+//
+//  Created by Tim Mitra on 2024-03-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Motion/MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionApp.swift
+//  Motion
+//
+//  Created by Tim Mitra on 2024-03-11.
+//
+
+import SwiftUI
+
+@main
+struct MotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
